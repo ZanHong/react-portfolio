3691ec2cc2f1f758d5770335fa9db2370f017c28
Zan Hong <mokzanhong92@hotmail.com>
2020-10-06 19:28:21 +1030

    Updates

---
+++ precache-manifest.f7b575167fc386dade744e92ccdef485.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2246b6338239b56a19eb3270d5b7db03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55b1dbb2ea2a57523d58",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4e0af7dd504816239f68",
+    "url": "/static/css/main.a6c668a8.chunk.css"
+  },
+  {
+    "revision": "55b1dbb2ea2a57523d58",
+    "url": "/static/js/2.c600feb2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c600feb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e0af7dd504816239f68",
+    "url": "/static/js/main.4f20b69b.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  }
+]);
